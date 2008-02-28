9909ff5e09a8f49b92afa3150830b384041e3589
kapilt <kapilt@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2008-02-28 07:40:20 +0000

    remove old comment

--- src/getpaid/ups/rates.py
+++ src/getpaid/ups/rates.py
@@ -67,10 +67,6 @@ class UPSRateService( Persistent, Contained ):
                                  order,          # destination contact and location
                                  pretty=True)
         
-        #Returns always a UPS response with two shipment rates (It takes too long to 
-        #register a UPS account
-        #return FakeResponse(request).shipments
-        #raise str(request)
         try:
             response_text = SendRequest( settings.server_url, request ).read()
         except URLError:
